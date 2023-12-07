79fbc05818f10a90da01d2acf8549de70c00d7f5
AmiRSamari <110278222+AmiRSamari@users.noreply.github.com>
2023-12-07 18:46:26 +0330

    Authentication state definition

--- E2- StudentCardManagementSystem/StudentCardManagementSystem.ino
+++ E2- StudentCardManagementSystem/StudentCardManagementSystem.ino
@@ -46,9 +46,9 @@ void loop() {
     case Authentication:
       AuthenticationState();
       break;
-    // case Reset:
-    //   Reset();
-    //   break;
+    case Reset:
+      ResetState();
+      break;
     // case Registration:
     //   Registration();
     //   break;
@@ -72,8 +72,9 @@ void StandbyState(){
     
     else if (command.equals("mfr -R")) {
       currentState = Reset;
-      // modeStartTime = millis();
+      registrationStart = millis(); // Record the start time of Reset state
       Serial.println("CurrentState: Reset.");
+      Serial.println("*Attention* You have only 10 seconds to put your Card on the RFID module");
     } 
     
     else if (command.startsWith("mfr -reg")) {
@@ -164,8 +165,54 @@ void AuthenticationState(){
 
 }
 
-void RestState(){
+void ResetState() {
+    if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+        MFRC522::StatusCode status;
 
+        // Prepare the key (used both as key A and as key B)
+        // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
+        MFRC522::MIFARE_Key key;
+        for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+        // Define empty data block
+        byte block[16];
+        for (byte i = 0; i < 16; i++) block[i] = 0;
+
+        // List of sectors to be made zero
+        byte sectors[] = {8, 9, 10};
+
+        for (int i = 0; i < sizeof(sectors); i++) {
+          // Authenticate using key A
+          status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, sectors[i], &key, &(mfrc522.uid));
+          if (status != MFRC522::STATUS_OK) {
+            Serial.print(F("Authentication failed: "));
+            Serial.println(mfrc522.GetStatusCodeName(status));
+            return;
+          }
+
+          // Write to sector
+          status = (MFRC522::StatusCode)mfrc522.MIFARE_Write(sectors[i], block, 16);
+          if (status != MFRC522::STATUS_OK) {
+            Serial.print(F("Writing failed: "));
+            Serial.println(mfrc522.GetStatusCodeName(status));
+            return;
+          }
+
+          Serial.print(F("Sector "));
+          Serial.print(sectors[i]);
+          Serial.println(F(" reset successful."));
+        }
+
+        // Halt PICC
+        mfrc522.PICC_HaltA();
+        // Stop encryption on PCD
+        mfrc522.PCD_StopCrypto1();
+    }
+
+    if (millis() - registrationStart >= REGISTRATION_DURATION) {
+        currentState = Standby;
+        Serial.println("Reset state timed out. Switched to Standby state.");
+    }
 }
 
 void RegistrationState(){
